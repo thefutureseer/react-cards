c1140eccb5ddc36f8d4f01938b5ae813a6c75413
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2021-04-03 17:33:45 -0700

    css

--- src/components/cardUi.jsx
+++ src/components/cardUi.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import horsy from '../css/images/babyHorse2.jpg';
 import "../css/card-style.css";
 
 function Card(props) {
   return (
     <div className='card text-center'>
       <div className="overflow">
-        <img src={horsy} alt="horsy" className="card-img-top"/>
+        <img src={props.imgsrc} className="card-img-top"/>
       </div>
       <div className="card-body text-dark">
-        <h4 className="card-title">CARD TITLE</h4>
+        <h4 className="card-title">{props.title}</h4>
         <p className="card-text text-secondary">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
            Consequatur labore nesciunt ratione consectetur reprehenderit 
            mollitia optio quia iure unde inventore!
         </p>
-        <a href="3" className="btn btn-outline-success">GO anywhere link!</a>
+        <a href="#" className="btn btn-outline-success">GO anywhere link!</a>
       </div>
        
     </div>
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
 import Card from "./cardUi";
+import horsy from "../css/images/babyHorse2.jpg";
+import babyWDogTwo from "../css/images/babyWDog2.jpg";
+import catScratch from "../css/images/catScratch.jpg";
 
 export default class Cards extends Component{
 
@@ -8,13 +11,13 @@ export default class Cards extends Component{
       <div className="container-fluid d-flex justify-content-center">
         <div className="row">
           <div className="col md 4">
-            <Card />
+            <Card imgsrc={horsy} title="Horsy"/>
           </div>
           <div className="col md 4">
-            <Card />
+            <Card imgsrc={babyWDogTwo} title="Two Babies"/>
           </div>
           <div className="col md 4">
-            <Card />
+            <Card imgsrc={catScratch} title="CatScratch"/>
           </div>
 
         </div>
